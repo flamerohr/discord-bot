3ef26d19b44d8dd34bd0be8ee82805f3ae6353b4
Chris Joe <flamer.ohr@gmail.com>
2021-04-29 13:57:14 +1200

    add prefix and ignore messages from bots

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const { prefix } = require('./config/bot');
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+client.on('message', (message) => {
+  // ignore other bots
+  if (message.author.bot) return;
+
+  // ignore non-prefixed messages
+  if (!message.content.startsWith(prefix)) return;
+
+  if (message.content === `${prefix}ping`) {
+    message.reply('pong');
   }
 });
 
