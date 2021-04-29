f92de3006d09ee7b64f973591aaff8a72fed2bbd
Chris Joe <flamer.ohr@gmail.com>
2021-04-30 10:21:57 +1200

    add emojis capabilities

--- commands/start.js
+++ commands/start.js
@@ -1,4 +1,8 @@
-module.exports = (client, message, { _ }) => {
+const emojis = require('../config/emojis');
+
+module.exports = (client, message, { _, mayreel }) => {
+  const emoji = mayreel ? ` ${emojis.bouncy}` : '';
+
   // just echo out what the message we had received
-  message.channel.send(`I got: ${_.join(' ')}`);
+  message.channel.send(`I start with${emoji}: ${_.join(' ')}`);
 };
---
+++ config/emojis.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // bot needs to be on my server to work
+  'bouncy': '<a:bouncy:830788783100264448>',
+};
