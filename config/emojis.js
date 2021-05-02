13b47b2e3d62143c37b4472559986099b473ea87
LAPTOP-4ONCPGNL\Mtran <mtran0011@outlook.com>
2021-05-02 19:09:45 +1000

    Updated the mayreel bouncy emoji.

--- config/emojis.js
+++ config/emojis.js
@@ -1,5 +1,5 @@
 module.exports = {
   // bot needs to be on my server to work
-  'bouncy': '<a:bouncy:830788783100264448>',
+  'bouncy': '<a:bouncy:838290465127399435>',
   'sohee': '<:Sohee:834658195518783488>',
 };
