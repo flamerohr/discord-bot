8be887ff80d80f9cf6004ea81830c0cf04469027
LAPTOP-4ONCPGNL\Mtran <mtran0011@outlook.com>
2021-05-02 16:14:06 +1000

    Added sohee emoji, which isn't an animated emoji.

--- commands/start.js
+++ commands/start.js
@@ -1,7 +1,12 @@
 const emojis = require('../config/emojis');
 
-module.exports = async (client, message, { _, mayreel }) => {
-  const emoji = mayreel ? ` ${emojis.bouncy}` : '';
+module.exports = async (client, message, { _, mayreel, sohee }) => {
+  let emoji = '';
+
+  if (mayreel)
+    emoji = ` ${emojis.bouncy}`;
+  else if (sohee)
+    emoji = ` ${emojis.sohee}`;
 
   // just echo out what the message we had received
   const botMessage = await message.channel.send(`I start with${emoji}: ${_.join(' ')}`);
--- config/emojis.js
+++ config/emojis.js
@@ -1,4 +1,5 @@
 module.exports = {
   // bot needs to be on my server to work
   'bouncy': '<a:bouncy:830788783100264448>',
+  'sohee': '<:Sohee:834658195518783488>',
 };
