6df28d5ee9900b1dafbb2fb03103eb0877ef315f
LAPTOP-4ONCPGNL\Mtran <mtran0011@outlook.com>
2021-07-25 11:13:38 +1000

    adjusted the environment variables for the chatroom app.

--- chat-room/scripts/api.js
+++ chat-room/scripts/api.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client';
 
-let socketIoServer = process.env.SOCKET_IO_SERVER ?? 'http://localhost:8000';
+let socketIoServer = process.env.NEXT_PUBLIC_SOCKET_IO_SERVER ?? 'http://localhost:8000';
 const socket = openSocket(socketIoServer);
 
 export {
